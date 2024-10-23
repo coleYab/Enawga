144d0e57787174cda53bdf330128840250b20aed
DanielJohn17 <danielyohannes38@gmail.com>
2024-10-23 17:11:47 +0300

    Added Shadcn

--- front-end/app/page.js
+++ front-end/app/page.js
@@ -1,3 +1,9 @@
+import { Button } from '@components/ui/button';
 export default function Home() {
-  return <h1 className="text-4xl font-bold text-green-500">Home</h1>;
+  return (
+    <div className="w-full min-h-screen p-8">
+      <h1 className="text-4xl font-bold text-green-500">Home</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
 }
