f49b7513b85203a96d222ec422e4517e81de0624
DanielJohn17 <danielyohannes38@gmail.com>
2024-10-27 20:32:53 +0300

    test page

--- front-end/app/page.js
+++ front-end/app/page.js
@@ -1,9 +1,9 @@
-import { Button } from '@components/ui/button';
 export default function Home() {
   return (
     <div className="w-full min-h-screen p-8">
-      <h1 className="text-4xl font-bold text-green-500">Home</h1>
-      <Button>Click Me</Button>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Home Page
+      </button>
     </div>
   );
 }
