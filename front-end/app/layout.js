9fcf6d4fad567a418e047c44630c78277f798667
DanielJohn17 <danielyohannes38@gmail.com>
2024-10-29 18:25:09 +0300

    Added tailwind styles

--- front-end/app/layout.js
+++ front-end/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="w-screen h-screen bg[var(--box-color)]">{children}</body>
     </html>
   );
 }
