2013296af328a043a7d42b748b14f811293b7fc8
DanielJohn17 <danielyohannes38@gmail.com>
2024-10-29 18:24:19 +0300

    Apllied Containers

--- front-end/app/home/page.jsx
+++ front-end/app/home/page.jsx
@@ -1,83 +1,31 @@
 'use client';
 import { useState } from 'react';
-import { IoSearchSharp } from 'react-icons/io5';
 
-import ProfileCard from '@components/ProfileCard';
-import InputCard from '@components/InputCard';
-import FriendCard from '@components/FriendCard';
-import ChatBubble from '@components/ChatBubble';
-import { MdSend } from 'react-icons/md';
-
-const mockUser = {
-  name: 'Filip',
-  bio: 'I am a software engineer',
-  time: '2:00 PM',
-  session: true,
-};
-
-const mockUser2 = {
-  name: 'Filip',
-  bio: 'I am a software engineer',
-  session: false,
-};
+import FriendList from '@containers/FriendList';
+import ChatBox from '@containers/ChatBox';
 
 const HomePage = () => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState('dark');
+  const [isBack, setIsBack] = useState(true);
 
   const changeTheme = () => {
-    document.body.classList.toggle('dark');
-    setTheme(theme === 'light' ? 'dark' : 'light');
+    document.body.classList.toggle('light');
+    setTheme(theme === 'dark' ? 'light' : 'dark');
   };
 
   return (
-    <div className="w-screen min-h-screen bg-[var(--background-color)] flex-center">
-      <div className="container">
-        <div className="left-side">
-          <div>
-            <ProfileCard
-              user={mockUser}
-              theme={theme}
-              changeTheme={changeTheme}
-            />
-          </div>
+    <div className="w-screen h-screen  bg-[var(--box-color)] ">
+      <div className="hidden lg:flex w-full h-full bg-[var(--box-color)] ">
+        <FriendList theme={theme} changeTheme={changeTheme} />
+        <ChatBox />
+      </div>
 
-          <div className="bg-[var(--box-color-3)] px-[13px] py-[10px]">
-            <div className="bg-[var(--box-color)] rounded-[40px] px-2 flex items-center">
-              <i className="text-2xl">
-                <IoSearchSharp />
-              </i>
-
-              <div className="w-full">
-                <InputCard placeHolder="Search here" />
-              </div>
-            </div>
-          </div>
-
-          <div className="chat-list">
-            <FriendCard user={mockUser} />
-            <hr />
-            <FriendCard user={mockUser} />
-            <hr />
-          </div>
-        </div>
-
-        <div className="right-side">
-          <div>
-            <ProfileCard user={mockUser2} />
-          </div>
-
-          <div className="px-3 py-6">
-            <ChatBubble message="Hello" session={true} />
-            <ChatBubble message="Hi, how are you?" session={false} />
-          </div>
-
-          <div className="flex-between px-4 py-3 absolute bottom-0 w-full bg-[var(--box-color-2)]">
-            <InputCard placeHolder="Text message" />
-            <i>
-              <MdSend size={25} />
-            </i>
-          </div>
-        </div>
+      <div className="lg:hidden block w-full h-full relative">
+        {!isBack ? (
+          <FriendList theme={theme} changeTheme={changeTheme} />
+        ) : (
+          <ChatBox />
+        )}
       </div>
     </div>
   );
