4d9aeb022c6599bc082304bcd7dcde47e87b0d6c
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2024-10-30 10:55:37 +0300

    google login routed added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,30 @@
 import express from 'express';
-import { login, signUp, logout } from '../controllers/auth.controller.js';
+import {
+  login,
+  signUp,
+  logout,
+  googleLogin,
+} from '../controllers/auth.controller.js';
+import passport from '../middleware/passportMiddleware.js';
 
 const router = express.Router();
 
 router.post('/login', login);
+
+router.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  }),
+);
+
+router.get(
+  '/loggedIn',
+  passport.authenticate('google', {
+    failureRedirect: '/api/auth/signup',
+  }),
+  googleLogin,
+);
 
 router.post('/signup', signUp);
 
