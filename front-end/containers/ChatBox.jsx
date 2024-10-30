278df00c96a803b238c7ccbf67158eeb32e98462
dyohannes764 <dyohannes764@gmail.com>
2024-10-30 18:12:18 +0000

    Added scroll behaviour and Friend list onClick functionality

--- front-end/containers/ChatBox.jsx
+++ front-end/containers/ChatBox.jsx
@@ -1,31 +1,59 @@
-'use client';
+"use client";
+import { useEffect, useRef } from "react";
+import { MdSend } from "react-icons/md";
 
-import { MdSend } from 'react-icons/md';
-
-import ProfileCard from '@components/ProfileCard';
-import ChatBubble from '@components/ChatBubble';
-import InputCard from '@components/InputCard';
+import ProfileCard from "@components/ProfileCard";
+import ChatBubble from "@components/ChatBubble";
+import InputCard from "@components/InputCard";
 
 const mockUser2 = {
-  name: 'Filip',
-  bio: 'I am a software engineer',
+  name: "Filip",
+  bio: "I am a software engineer",
   session: false,
 };
 
-const ChatBox = () => {
+const ChatBox = ({ changeBack }) => {
+  const chatContainerRef = useRef(null);
+
+  useEffect(() => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
+    }
+  });
+
   return (
-    <div className="right-side border-[5px] border-green-200 ">
-      <div>
-        <ProfileCard user={mockUser2} />
+    <div className="right-side">
+      <div className="mb-5">
+        <ProfileCard user={mockUser2} changeBack={changeBack} />
       </div>
 
-      <div className="flex flex-col gap-4 px-3 py-6">
-        <ChatBubble message="Hello" session={true} />
-        <ChatBubble message="Hi, how are you?" session={false} />
+      <div className="flex-column overflow-y-scroll">
+        <div className="px-4 py-6 flex-column gap-5" ref={chatContainerRef}>
+          <ChatBubble message="Hello" session={true} />
+          <ChatBubble message="Hello" session={false} />
+          <ChatBubble message="Hello" session={true} />
+          <ChatBubble message="Hello" session={false} />
+          <ChatBubble message="Hello" session={true} />
+          <ChatBubble message="Hello" session={false} />
+          <ChatBubble message="Hello" session={false} />
+          <ChatBubble message="Hello" session={false} />
+          <ChatBubble message="Hello" session={false} />
+          <ChatBubble message="Hello" session={false} />
+          <ChatBubble message="Hello" session={false} />
+          <ChatBubble message="Hello" session={false} />
+          <ChatBubble message="Hello" session={false} />
+          <ChatBubble message="Hello" session={true} />
+          <ChatBubble message="Hello" session={true} />
+          <ChatBubble message="Hello" session={true} />
+          <ChatBubble message="Hello" session={true} />
+        </div>
       </div>
-
-      <div className="absolute bottom-0 bg-white">
+      <div className="px-[16px] py-[16px] bg-[var(--box-color-2)] flex-between">
         <InputCard placeHolder="Text Message" />
+        <i>
+          <MdSend size={25} />
+        </i>
       </div>
     </div>
   );
--- front-end/containers/FriendList.jsx
+++ front-end/containers/FriendList.jsx
@@ -1,23 +1,28 @@
-'use client';
+"use client";
 
-import { IoSearchSharp } from 'react-icons/io5';
+import { IoSearchSharp } from "react-icons/io5";
 
-import ProfileCard from '@components/ProfileCard';
-import InputCard from '@components/InputCard';
-import FriendCard from '@components/FriendCard';
+import ProfileCard from "@components/ProfileCard";
+import InputCard from "@components/InputCard";
+import FriendCard from "@components/FriendCard";
 
 const mockUser = {
-  name: 'Filip',
-  bio: 'I am a software engineer',
-  time: '2:00 PM',
+  name: "Filip",
+  bio: "I am a software engineer",
+  time: "2:00 PM",
   session: true,
 };
 
-const FriendList = ({ theme, changeTheme }) => {
+const FriendList = ({ theme, changeTheme, handleNotification }) => {
   return (
     <div className="left-side">
       <div>
-        <ProfileCard user={mockUser} theme={theme} changeTheme={changeTheme} />
+        <ProfileCard
+          user={mockUser}
+          theme={theme}
+          changeTheme={changeTheme}
+          handleNotification={handleNotification}
+        />
       </div>
 
       <div className="bg-[var(--box-color-3)] px-[13px] py-[10px]">
@@ -32,11 +37,21 @@ const FriendList = ({ theme, changeTheme }) => {
         </div>
       </div>
 
-      <div className="chat-list">
-        <FriendCard user={mockUser} />
-        <hr />
-        <FriendCard user={mockUser} />
-        <hr />
+      <div className="h-full overflow-y-auto">
+        <div
+          onClick={() => {
+            document
+              .getElementById("profile_container")
+              .classList.replace("hidden", "fixed");
+          }}
+        >
+          <FriendCard user={mockUser} />
+          <hr />
+        </div>
+        <div>
+          <FriendCard user={mockUser} />
+          <hr />
+        </div>
       </div>
     </div>
   );
