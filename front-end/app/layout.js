2c4d95d9af56982e949c211769d8462d0ff86707
dyohannes764 <dyohannes764@gmail.com>
2024-10-30 18:17:04 +0000

    minor tailwind css fix

--- front-end/app/layout.js
+++ front-end/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="w-screen h-screen bg[var(--box-color)]">{children}</body>
+      <body className="w-screen h-screen bg[var(--background-color)]">{children}</body>
     </html>
   );
 }
--- front-end/app/page.js
+++ front-end/app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="w-full min-h-screen p-8">
+    <div className="w-full h-screen p-8">
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Home Page
       </button>
