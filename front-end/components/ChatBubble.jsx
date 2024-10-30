92f0558f4935c6d0bc2d48e0957aacf92d1bdff7
dyohannes764 <dyohannes764@gmail.com>
2024-10-30 18:15:09 +0000

    Some tailwind hight related minor fix

--- front-end/components/ChatBubble.jsx
+++ front-end/components/ChatBubble.jsx
@@ -2,11 +2,11 @@ const ChatBubble = ({ message, session }) => {
   return (
     <div>
       {session ? (
-        <div className="flex justify-end">
-          <div className="chat-bubble bubble-right ">{message}</div>
+        <div className="w-full h-auto flex justify-end">
+          <div className="chat-bubble bubble-right self-end">{message}</div>
         </div>
       ) : (
-        <div className="flex justify-start">
+        <div className="w-full h-auto flex justify-start">
           <div className="chat-bubble bubble-left">{message}</div>
         </div>
       )}
--- front-end/components/InputCard.jsx
+++ front-end/components/InputCard.jsx
@@ -2,7 +2,7 @@ const InputCard = ({ placeHolder }) => {
   return (
     <div className="w-full">
       <input
-        className="w-[80%] px-5 py-1 border-0 rounded-[30px] text-wrap text-[var(--text-color)] bg-[var(--box-color)] focus:outline-none"
+        className="w-[80%] px-5 py-1 border-0 rounded-[30px] text-wrap text-[var(--text-color)] bg-[var(--box-color)] focus:outline-none transition-colors"
         type="text"
         placeholder={placeHolder}
       />
