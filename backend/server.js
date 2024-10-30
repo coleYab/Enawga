e4ade94d7ecd31902af55d44b09633635a170a8b
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2024-10-30 11:07:38 +0300

    express server

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,52 @@
-import express from 'express';
 import dotenv from 'dotenv';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import session from 'express-session';
+
+import messageRoutes from './routes/message.routes.js';
+import authRoutes from './routes/auth.routes.js';
+import userRoutes from './routes/user.routes.js';
+import searchRoutes from './routes/search.routes.js';
+
+import connectToMongoDB from './db/connectToMongoDB.js';
+import logServerRequests from './middleware/logServerRequests.js';
+import passport from './middleware/passportMiddleware.js';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const GOOGLE_SECRET = process.env.GOOGLE_SECRET;
 
 dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 3000;
+// Middleware to log every request with status code and duration
+app.use(logServerRequests);
+app.use(express.json()); // parse incoming requests with JSON payloads(req.body or data)
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser()); // parse cookies attached to the client request
+app.use(cors());
 
-app.get('/', () => {
-  console.log('Hello express');
+app.use(
+  session({
+    secret: GOOGLE_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 },
+  }),
+);
+
+// Setting up passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/search', searchRoutes);
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`The server is running on port ${PORT}`);
 });
 
-app.listen(port, () => {
-  console.log('Server running at port: port');
-});
+export default app;
