68c3dd1e7870a1abf83b836f320f0f8c94b7777f
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2024-10-31 23:30:44 +0300

    verifyToken added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from '../models/user.model.js';
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 import generateTokenAndSetCookie from '../utils/generateToken.js';
 
@@ -131,5 +132,30 @@ export const logout = async (req, res) => {
   } catch (error) {
     console.error('Error Logout controller:', error.message);
     res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+export const verifyToken = async (req, res) => {
+  try {
+    const token = req.cookies.jwt;
+
+    if (!token) {
+      return res.status(401).json({ error: 'No token provided' });
+    }
+
+    // Verify the token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId).select(
+      '_id fullName username email profilePic bio',
+    );
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    res.status(200).json({ user });
+  } catch (error) {
+    console.error('Error in token verification:', error.message);
+    res.status(403).json({ error: 'Invalid or expired token' });
   }
 };
