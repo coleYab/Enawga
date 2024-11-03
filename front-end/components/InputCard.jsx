ee29a846130ea6fe477b8836b44f246b68232650
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2024-11-03 21:11:52 +0300

    functionality added

--- front-end/components/InputCard.jsx
+++ front-end/components/InputCard.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react';
 
-const InputCard = ({ placeHolder }) => {
-  const [message, setMessage] = useState('');
-  const handleMessage = () => {};
+const InputCard = ({ placeHolder, inputChange, inputValue }) => {
   return (
     <div className="w-full">
       <input
         className="w-[80%] px-5 py-1 border-0 rounded-[30px] text-wrap text-[var(--text-color)] bg-[var(--box-color)] focus:outline-none transition-colors"
         type="text"
         placeholder={placeHolder}
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
+        value={inputValue}
+        onChange={inputChange}
       />
     </div>
   );
