db28157799731cf1c7f4ca68f791bf0c6569a4c0
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2024-11-03 21:10:54 +0300

    minor update

--- front-end/app/home/page.jsx
+++ front-end/app/home/page.jsx
@@ -1,102 +1,129 @@
-"use client";
-import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
+'use client';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
 
-import FriendList from "@containers/FriendList";
-import ChatBox from "@containers/ChatBox";
-import NotificationList from "@containers/NotificationList";
-import SideBar from "@containers/SideBar";
-import ProfileContainer from "@containers/ProfileContainer";
+import FriendList from '@containers/FriendList';
+import ChatBox from '@containers/ChatBox';
+import NotificationList from '@containers/NotificationList';
+import SideBar from '@containers/SideBar';
+import ProfileContainer from '@containers/ProfileContainer';
+import Error from '@containers/ErrorPage';
 
 const mockUsers = [
   {
-    name: "Filip",
-    bio: "I am a software engineer",
-    time: "2:00 PM",
+    name: 'Filip',
+    bio: 'I am a software engineer',
+    time: '2:00 PM',
     session: true,
   },
   {
-    name: "Jonah",
+    name: 'Jonah',
     bio: "Don't focus under the sun",
-    time: "3:21 AM",
+    time: '3:21 AM',
     session: true,
   },
   {
-    name: "Dagi",
-    bio: "A Proud lua developer",
-    time: "6:41 AM",
+    name: 'Dagi',
+    bio: 'A Proud lua developer',
+    time: '6:41 AM',
     session: true,
   },
 
   {
-    name: "Levon",
-    bio: "Tried to be a chess champion",
-    time: "1:25 PM",
+    name: 'Levon',
+    bio: 'Tried to be a chess champion',
+    time: '1:25 PM',
     session: true,
   },
   {
-    name: "Terminator",
-    bio: "With the necessary effort everything is possible.",
-    time: "3:21 AM",
+    name: 'Terminator',
+    bio: 'With the necessary effort everything is possible.',
+    time: '3:21 AM',
     session: true,
   },
 ];
 
 const messageList = [
   {
-    message: "Hello",
-    username: "Daniel",
-    time: "2:40AM",
+    message: 'Hello',
+    username: 'Daniel',
+    time: '2:40AM',
     session: false,
   },
   {
-    message: "Hello",
-    username: "Nati",
-    time: "3:00PM",
+    message: 'Hello',
+    username: 'Nati',
+    time: '3:00PM',
     session: false,
   },
   {
-    message: "How was the program?",
-    username: "Nati",
-    time: "3:02PM",
+    message: 'How was the program?',
+    username: 'Nati',
+    time: '3:02PM',
     session: false,
   },
   {
-    message: "Which one are you asking?",
-    username: "Daniel",
-    time: "3:07PM",
+    message: 'Which one are you asking?',
+    username: 'Daniel',
+    time: '3:07PM',
     session: false,
   },
 ];
 
 const HomePage = () => {
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState('dark');
   const [isBack, setIsBack] = useState(true);
   const [openNoti, setOpenNoti] = useState(false);
   const [currentUser, setCurrentUser] = useState({});
+  const [loading, setLoading] = useState(true);
 
   const router = useRouter();
 
-  // useEffect(() => {
-  //   const fetchCurrentUser = async () => {
-  //     const response = await fetch("http://localhost:3000/api/auth/verify", {
-  //       credentials: "include",
-  //     });
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      try {
+        // const response = await fetch('http://localhost:5000/api/auth/verify', {
+        //   headers: {
+        //     credentials: 'include',
+        //   },
+        // });
+        // const response = await axios.get(
+        //   'http://localhost:5000/api/auth/verify',
+        //   {
+        //     withCredentials: true, // This allows cookies to be sent with the request
+        //   },
+        // );
+        const response = await axios.get(
+          `http://localhost:5000/api/auth/verify?timestamp=${new Date().getTime()}`, // To prevent caching.
+          {
+            withCredentials: true,
+          },
+        );
+        console.log(response.status);
+        if (response.status === 200) {
+          const data = response.data;
+          console.log('Home Data: ', data.user);
+          setCurrentUser(data.user);
+          setLoading(false);
+        } else {
+          console.log('No user data found');
+          setCurrentUser(null);
+          setLoading(false);
+        }
+      } catch (error) {
+        setCurrentUser(null);
+        setLoading(false);
+        console.log(error);
+      }
+    };
 
-  //     if (!response.ok) {
-  //       router.push("/login");
-  //     }
-
-  //     const data = await response.json();
-  //     setCurrentUser(data.user);
-  //   };
-
-  //   fetchCurrentUser();
-  // }, []);
+    fetchCurrentUser();
+  }, []);
 
   const changeTheme = () => {
-    document.body.classList.toggle("light");
-    setTheme(theme === "dark" ? "light" : "dark");
+    document.body.classList.toggle('light');
+    setTheme(theme === 'dark' ? 'light' : 'dark');
   };
 
   const changeBack = () => {
@@ -108,36 +135,49 @@ const HomePage = () => {
   };
 
   return (
-    <div className="w-screen h-screen bg-[var(--box-color)] flex justify-center">
-      <SideBar user={mockUsers[0]} />
-      <ProfileContainer user={mockUsers[0]} />
+    <>
+      {loading ? (
+        <h1 className="text-black">Loading ...</h1>
+      ) : !currentUser ? (
+        <>
+          <Error message="User data not found." />
+          <p className="text-black">
+            Try Logging in. <a href="/login">Login</a>
+          </p>
+        </>
+      ) : (
+        <div className="w-screen h-screen bg-[var(--box-color)] flex justify-center">
+          <SideBar user={mockUsers[0]} />
+          <ProfileContainer user={mockUsers[0]} />
 
-      {openNoti && (
-        <NotificationList
-          messageList={messageList}
-          handleNotification={handleNotification}
-        />
+          {openNoti && (
+            <NotificationList
+              messageList={messageList}
+              handleNotification={handleNotification}
+            />
+          )}
+
+          <div className="hidden lg:flex w-screen h-screen bg-[var(--box-color)] z-0">
+            <FriendList
+              users={mockUsers}
+              theme={theme}
+              changeTheme={changeTheme}
+              handleNotification={handleNotification}
+              currentUser={currentUser}
+            />
+            <ChatBox />
+          </div>
+
+          <div className="lg:hidden block w-full h-full relative">
+            {!isBack ? (
+              <FriendList theme={theme} changeTheme={changeTheme} />
+            ) : (
+              <ChatBox changeBack={changeBack} />
+            )}
+          </div>
+        </div>
       )}
-
-      <div className="hidden lg:flex w-screen h-screen bg-[var(--box-color)] z-0">
-        <FriendList
-          users={mockUsers}
-          theme={theme}
-          changeTheme={changeTheme}
-          handleNotification={handleNotification}
-          currentUser={currentUser}
-        />
-        <ChatBox />
-      </div>
-
-      <div className="lg:hidden block w-full h-full relative">
-        {!isBack ? (
-          <FriendList theme={theme} changeTheme={changeTheme} />
-        ) : (
-          <ChatBox changeBack={changeBack} />
-        )}
-      </div>
-    </div>
+    </>
   );
 };
 
