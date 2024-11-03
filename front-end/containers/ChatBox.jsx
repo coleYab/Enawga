37f2c628fa6a33613336c8dd77f0670d6936dfad
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2024-11-03 21:12:37 +0300

    minor update

--- front-end/containers/ChatBox.jsx
+++ front-end/containers/ChatBox.jsx
@@ -1,30 +1,37 @@
-"use client";
+'use client';
 
-import { useState, useEffect, useRef } from "react";
-import { MdSend } from "react-icons/md";
+import { useState, useEffect, useRef } from 'react';
+import { MdSend } from 'react-icons/md';
 
-import ProfileCard from "@components/ProfileCard";
-import ChatBubble from "@components/ChatBubble";
-import InputCard from "@components/InputCard";
+import ProfileCard from '@components/ProfileCard';
+import ChatBubble from '@components/ChatBubble';
+import InputCard from '@components/InputCard';
 
 import { initializeSocket, disconnectSocket } from '../utils/socket.js';
 
 const mockUser2 = {
-  name: "Filip",
-  bio: "I am a software engineer",
+  name: 'Filip',
+  bio: 'I am a software engineer',
   session: false,
 };
 
 const ChatBox = ({ changeBack }) => {
   const chatContainerRef = useRef(null);
   const [messages, setMessages] = useState([]);
+  const [textValue, setTextValue] = useState('');
   const socket = useRef(initializeSocket());
+
+  const handleChange = (e) => {
+    setTextValue(e.target.value);
+    console.log(textValue);
+  };
 
   // make a request and when a request is we can change the user
   const sendMessage = () => {
-    const message =  "Sending the message from Input Card";
+    const message = 'Sending the message from Input Card';
     socket.current.emit('sendMessage', message);
-    setMessages(messages => [...messages, { message, session: true }]);
+    setMessages((messages) => [...messages, { message, session: true }]);
+    setTextValue('');
   };
 
   useEffect(() => {
@@ -35,7 +42,10 @@ const ChatBox = ({ changeBack }) => {
     });
 
     currentSocket.on('recieveMessage', (message) => {
-      setMessages((prevMessages) => [...prevMessages, { message, session: false }]);
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { message, session: false },
+      ]);
     });
 
     return () => {
@@ -59,13 +69,19 @@ const ChatBox = ({ changeBack }) => {
       <div className="flex-column overflow-y-scroll">
         <div className="px-4 py-6 flex-column gap-5" ref={chatContainerRef}>
           {messages.map((msg, index) => (
-            <ChatBubble key={index} message={msg.message} session={msg.session} />
+            <ChatBubble
+              key={index}
+              message={msg.message}
+              session={msg.session}
+            />
           ))}
         </div>
       </div>
       <div className="px-[16px] py-[16px] bg-[var(--box-color-2)] flex-between">
         <InputCard
           placeHolder="Text Message"
+          inputChange={handleChange}
+          inputValue={textValue}
         />
         <i onClick={sendMessage}>
           <MdSend size={25} />
