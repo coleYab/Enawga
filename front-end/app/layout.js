1d37a56ab80915a6d066a1dd9530ab21f8298c94
DanielJohn17 <danielyohannes38@gmail.com>
2024-11-05 20:35:12 +0300

    Added loading screen without functionality

--- front-end/app/layout.js
+++ front-end/app/layout.js
@@ -1,14 +1,19 @@
-import '@styles/globals.css';
+import "@styles/globals.css";
 
 export const metadata = {
-  title: 'Enawga',
-  description: 'Chat app',
+  title: "Enawga",
+  description: "Chat app",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="w-screen h-screen bg[var(--background-color)]">{children}</body>
+      <body className="w-screen h-screen bg[var(--background-color)]">
+        {children}
+        <div id="loading-body">
+          <div id="loading-spinner" />
+        </div>
+      </body>
     </html>
   );
 }
