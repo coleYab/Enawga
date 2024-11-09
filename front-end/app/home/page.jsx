7e87d90cd24abc1d3e0f75dc7f0ab1b9785539ce
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2024-11-09 21:38:54 +0300

    minor fix

--- front-end/app/home/page.jsx
+++ front-end/app/home/page.jsx
@@ -1,78 +1,17 @@
-"use client";
-import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
-import axios from "axios";
+'use client';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
 
-import FriendList from "@containers/FriendList";
-import ChatBox from "@containers/ChatBox";
-import NotificationList from "@containers/NotificationList";
-import SideBar from "@containers/SideBar";
-import SearchUsers from "@containers/SearchUsers";
-import Error from "@containers/ErrorPage";
-
-const mockUsers = [
-  {
-    name: "Filip",
-    bio: "I am a software engineer",
-    time: "2:00 PM",
-    session: true,
-  },
-  {
-    name: "Jonah",
-    bio: "Don't focus under the sun",
-    time: "3:21 AM",
-    session: true,
-  },
-  {
-    name: "Dagi",
-    bio: "A Proud lua developer",
-    time: "6:41 AM",
-    session: true,
-  },
-
-  {
-    name: "Levon",
-    bio: "Tried to be a chess champion",
-    time: "1:25 PM",
-    session: true,
-  },
-  {
-    name: "Terminator",
-    bio: "With the necessary effort everything is possible.",
-    time: "3:21 AM",
-    session: true,
-  },
-];
-
-const messageList = [
-  {
-    message: "Hello",
-    username: "Daniel",
-    time: "2:40AM",
-    session: false,
-  },
-  {
-    message: "Hello",
-    username: "Nati",
-    time: "3:00PM",
-    session: false,
-  },
-  {
-    message: "How was the program?",
-    username: "Nati",
-    time: "3:02PM",
-    session: false,
-  },
-  {
-    message: "Which one are you asking?",
-    username: "Daniel",
-    time: "3:07PM",
-    session: false,
-  },
-];
+import FriendList from '@containers/FriendList';
+import ChatBox from '@containers/ChatBox';
+import NotificationList from '@containers/NotificationList';
+import SideBar from '@containers/SideBar';
+import SearchUsers from '@containers/SearchUsers';
+import Error from '@containers/ErrorPage';
 
 const HomePage = () => {
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState('dark');
   const [isBack, setIsBack] = useState(true);
   const [openNoti, setOpenNoti] = useState(false);
   const [currentUser, setCurrentUser] = useState({});
@@ -85,31 +24,20 @@ const HomePage = () => {
   useEffect(() => {
     const fetchCurrentUser = async () => {
       try {
-        // const response = await fetch('http://localhost:5000/api/auth/verify', {
-        //   headers: {
-        //     credentials: 'include',
-        //   },
-        // });
-        // const response = await axios.get(
-        //   'http://localhost:5000/api/auth/verify',
-        //   {
-        //     withCredentials: true, // This allows cookies to be sent with the request
-        //   },
-        // );
         const response = await axios.get(
           `http://localhost:5000/api/auth/verify?timestamp=${new Date().getTime()}`, // To prevent caching.
           {
             withCredentials: true,
-          }
+          },
         );
         console.log(response.status);
         if (response.status === 200) {
           const data = response.data;
-          console.log("Home Data: ", data.user);
+          console.log('Home Data: ', data.user);
           setCurrentUser(data.user);
           setLoading(false);
         } else {
-          console.log("No user data found");
+          console.log('No user data found');
           setCurrentUser(null);
           setLoading(false);
         }
@@ -126,23 +54,23 @@ const HomePage = () => {
   useEffect(() => {
     const fetchFriends = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/users", {
-          credentials: "include",
+        const response = await fetch('http://localhost:5000/api/users', {
+          credentials: 'include',
         });
 
         if (!response.ok) {
-          console.log("Failed to fetch friends");
+          console.log('Failed to fetch friends');
           return;
         }
 
         const data = await response.json();
 
         if (data) {
-          console.log("Friends: ", data);
+          console.log('Friends: ', data);
           setFriends(data);
         }
       } catch (error) {
-        console.log("Error fetching friends: ", error);
+        console.log('Error fetching friends: ', error);
       }
     };
 
@@ -150,8 +78,8 @@ const HomePage = () => {
   }, []);
 
   const changeTheme = () => {
-    document.body.classList.toggle("light");
-    setTheme(theme === "dark" ? "light" : "dark");
+    document.body.classList.toggle('light');
+    setTheme(theme === 'dark' ? 'light' : 'dark');
   };
 
   const changeBack = () => {
@@ -163,9 +91,9 @@ const HomePage = () => {
   };
 
   const handleClickedUser = (user) => {
-    console.log("Clicked User: ", user);
+    console.log('Clicked User: ', user);
     setClickedUser(user);
-    console.log("Set Clicked User: ", clickedUser);
+    console.log('Set Clicked User: ', clickedUser);
   };
 
   return (
