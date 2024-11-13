b233e2f6cf97842ada969be3f28de3ef72b4c17f
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2024-11-13 19:39:20 +0300

    sidebar edit done

--- front-end/containers/SideBar.jsx
+++ front-end/containers/SideBar.jsx
@@ -1,16 +1,17 @@
-"use client";
-import { useEffect, useState } from "react";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+'use client';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
-import { IoArrowBack } from "react-icons/io5";
-import { GiExitDoor } from "react-icons/gi";
+import { IoArrowBack } from 'react-icons/io5';
+import { GiExitDoor } from 'react-icons/gi';
 
-import { sleep } from "@utils/commonFunctions";
+import { sleep } from '@utils/commonFunctions';
+import DefaultProfile from '@public/assets/default-profile-image.jpg';
 
 const SideBar = ({ currentUser }) => {
   const [bio, setBio] = useState(currentUser.bio);
-  const [debounceBio, setDebounceBio] = useState("");
+  const [debounceBio, setDebounceBio] = useState('');
   const router = useRouter();
 
   // useEffect(() => {
@@ -52,26 +53,26 @@ const SideBar = ({ currentUser }) => {
 
   const handleLogout = async () => {
     try {
-      const response = await fetch("http://localhost:5000/api/auth/logout", {
-        method: "DELETE",
-        credentials: "include",
+      const response = await fetch('http://localhost:5000/api/auth/logout', {
+        method: 'DELETE',
+        credentials: 'include',
       });
       if (!response.ok) {
-        console.log("Logout Failed!");
+        console.log('Logout Failed!');
         return;
       }
 
       sleep(500);
-      document.getElementById("loading-body").classList.add("loading-body");
+      document.getElementById('loading-body').classList.add('loading-body');
       document
-        .getElementById("loading-spinner")
-        .classList.add("loading-spinner");
-      router.push("/");
+        .getElementById('loading-spinner')
+        .classList.add('loading-spinner');
+      router.push('/');
       sleep(500);
-      document.getElementById("loading-body").classList.remove("loading-body");
+      document.getElementById('loading-body').classList.remove('loading-body');
       document
-        .getElementById("loading-spinner")
-        .classList.remove("loading-spinner");
+        .getElementById('loading-spinner')
+        .classList.remove('loading-spinner');
     } catch (error) {
       console.log(error);
     }
@@ -86,8 +87,8 @@ const SideBar = ({ currentUser }) => {
         <i
           onClick={() => {
             document
-              .getElementById("sidebar")
-              .classList.replace("left-0", "-left-[100%]");
+              .getElementById('sidebar')
+              .classList.replace('left-0', '-left-[100%]');
           }}
         >
           <IoArrowBack size={25} />
@@ -97,7 +98,7 @@ const SideBar = ({ currentUser }) => {
           src={
             currentUser?.profilePic
               ? `${currentUser.profilePic}`
-              : `https://www.clarity-enhanced.net/wp-content/uploads/2020/06/filip.jpg`
+              : DefaultProfile
           }
           alt="profile image"
           width={50}
@@ -106,21 +107,29 @@ const SideBar = ({ currentUser }) => {
         />
       </div>
 
-      <div className="flex-column gap-5 px-4 py-4">
-        <h2 className="font-bold text-xl lg:text-2xl w-full ml-[1rem]">
-          {currentUser.username}
-        </h2>
-
-        <div>
-          <textarea
-            cols="30"
-            rows="5"
-            value={bio}
-            // onChange={(e) => setBio((prev) => e.target.value)}
-            onChange={(e) => setBio(e.target.value)}
-            className="text-[1.15rem] w-full max-h-[300px] px-5 py-3 border-0 rounded-[30px] text-wrap text-[var(--text-color)] bg-[var(--box-color-2)] focus:outline-none transition-colors"
-          ></textarea>
+      <div className="flex-col h-screen gap-8 px-4 py-4">
+        <div className="bg-gray-500 flex flex-column gap-2 p-5 rounded-lg min-w-fit max-w-[70%]">
+          <h2 className="font-bold text-2xl lg:text-2xl w-full ml-[1rem]">
+            <span className="text-2xl text-blue-300">Name: </span>
+            {currentUser.fullName}
+          </h2>
+          <h2 className="font-bold text-2xl lg:text-2xl w-full ml-[1rem]">
+            <span className="text-2xl text-blue-300">Username:</span>{' '}
+            {currentUser.username}
+          </h2>
+          {currentUser.bio && (
+            <h2 className="font-bold text-2xl lg:text-2xl w-full ml-[1rem]">
+              <span className="text-2xl text-blue-300">Bio:</span>{' '}
+              {currentUser.bio}
+            </h2>
+          )}
         </div>
+        <button
+          className="text-2xl py-3 px-6 bg-blue-500 my-4 rounded-lg hover:bg-blue-400"
+          onClick={() => router.push('/edit-profile')}
+        >
+          Edit Profile
+        </button>
       </div>
 
       <div
